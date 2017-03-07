0a2981a1cef5e6613ea5d2a745996591f1f501c5
beiquan021 <yangjingjing>
2017-03-07 14:43:24 +0800

    vue for form

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -2,12 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import ElementUI from 'element-ui'
+import Vuex from 'vuex'
 import App from './App'
 import router from './router'
 import VueResource from 'vue-resource'
 
 Vue.use(VueResource)
 Vue.use(ElementUI)
+Vue.use(Vuex)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
